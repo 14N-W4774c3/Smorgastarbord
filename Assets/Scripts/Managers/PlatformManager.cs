a5884b14cf9ed584a4e6097272044bb848aa8b38
Carolyn-Hope256 <cmhope@ucsc.edu>
2024-12-08 16:38:42 -0800

    imported basic 3D platformer framework that allows for varied varied platforming effects.

---
+++ Assets/Scripts/Managers/PlatformManager.cs
@@ -0,0 +1,51 @@
+using System;
+using UnityEngine;
+
+//Monobehavior script for storing different potential properties of platforms. All collision-based events are handled on the playermovement end.
+public class PlatformManager : MonoBehaviour
+{
+    [Header("Bouncy Platforms")]
+    public float bounciness;
+
+    [Header("Moving Platforms")]
+    public float moveRangeX;
+    public float moveSpeed;
+    private int backtracking = 1;
+    private float offset = 0;
+
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private void FixedUpdate()
+    {
+        if (moveRangeX > 0 && moveSpeed > 0)
+        {
+            platformMove();
+        }
+    }
+
+
+    private void platformMove()
+    {
+        transform.Translate(backtracking * moveSpeed * Time.deltaTime,0,0);
+        offset += backtracking * moveSpeed * Time.deltaTime;
+        if (offset > moveRangeX / 2)
+        {
+            backtracking = -1;
+        }
+        if (offset < -moveRangeX / 2)
+        {
+            backtracking = 1;
+        }
+
+    }
+}
