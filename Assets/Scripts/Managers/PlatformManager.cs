18d47d889c83f74dd9ab7569ac4ee09b29f238b3
Ian Wallace <iawallac@ucsc.edu>
2024-12-10 19:13:30 -0800

    Removed unnecessary function calls from Platform Manager.

--- Assets/Scripts/Managers/PlatformManager.cs
+++ Assets/Scripts/Managers/PlatformManager.cs
@@ -13,18 +13,6 @@ public class PlatformManager : MonoBehaviour
     private int backtracking = 1;
     private float offset = 0;
 
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
-    void Start()
-    {
-        
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
-
     private void FixedUpdate()
     {
         if (moveRangeX > 0 && moveSpeed > 0)
